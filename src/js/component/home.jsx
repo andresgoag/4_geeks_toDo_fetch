df715f18a61e85f4a95f26c5529ffebde390e777
andresgoag <andresgomezagudelo@hotmail.com>
2021-10-20 18:17:49 +0000

    todo with fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 import { ToDoList } from "./todolist.jsx";
 
 //create your first component
 const Home = () => {
 	return (
-		<div>
+		<div className="home-container">
 			<ToDoList />
 		</div>
 	);
--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-import { getTodos, updateTodos } from "../api";
+import { getTodos } from "../api";
 
 export const ToDoList = () => {
+	const user = "andresgoag";
 	const [newTask, setNewTask] = React.useState("");
-	const [listOfTasks, setListOfTasks] = React.useState([]);
+	const [listOfTasks, setListOfTasks] = React.useState(null);
+
+	const newTaskKeyDownHandler = e => {
+		if (e.key === "Enter") {
+			if (newTask === "") {
+				alert("Please enter a task");
+			} else {
+				addToList();
+			}
+		}
+	};
 
 	const addToList = () => {
-		setListOfTasks([...listOfTasks, newTask]);
+		setListOfTasks([...listOfTasks, { label: newTask, done: false }]);
 		setNewTask("");
 	};
 
@@ -19,66 +30,85 @@ export const ToDoList = () => {
 	};
 
 	React.useEffect(() => {
-		const fn = async () => {
-			const todos = await getTodos();
-			setListOfTasks(todos.map(item => item.label));
-		};
-		fn();
+		fetch(`https://assets.breatheco.de/apis/fake/todos/user/${user}`)
+			.then(response => {
+				if (response.status == 200) {
+					return response.json();
+				} else {
+					throw new Error("Error getting data from the server");
+				}
+			})
+			.then(data => setListOfTasks(data))
+			.catch(error => console.error(error));
 	}, []);
 
 	React.useEffect(() => {
-		const fn = async () => {
-			updateTodos(
-				listOfTasks.map(item => ({ label: item, done: false }))
-			);
+		const postData = () => {
+			fetch(`https://assets.breatheco.de/apis/fake/todos/user/${user}`, {
+				method: "PUT",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(listOfTasks)
+			});
 		};
+
 		if (listOfTasks !== null) {
-			fn();
+			if (listOfTasks.length == 0) {
+				fetch(
+					`https://assets.breatheco.de/apis/fake/todos/user/${user}`,
+					{
+						method: "DELETE",
+						headers: { "Content-Type": "application/json" }
+					}
+				);
+			} else {
+				postData();
+			}
 		}
 	}, [listOfTasks]);
 
 	return (
-		<div className="notePad container-fluid">
-			<div className="text-center mt-5">
-				<h1>To Do List</h1>
+		<div className="main-container">
+			<div>
+				<h1>Tasks to do</h1>
 			</div>
 			<input
 				placeholder="Add new task"
-				className="container"
+				className="new-task-input"
 				type="text"
 				value={newTask}
-				onChange={event => setNewTask(event.target.value)}
-				onKeyUp={event => {
-					if (event.key === "Enter") {
-						if (newTask === "") {
-							alert("Please enter a task");
-						} else {
-							addToList();
-						}
-					}
-				}}
+				onChange={e => setNewTask(e.target.value)}
+				onKeyDown={newTaskKeyDownHandler}
 			/>
-			{listOfTasks.length == 0 ? <p>No tasks, add a task</p> : null}
 
-			{listOfTasks.map((item, index) => {
-				return (
-					<div className="todo" key={index}>
-						<p className="todo_content">{item}</p>
-						<p
-							className="todo_delete"
-							onClick={() => {
-								deleteTask(index);
-							}}>
-							<i className="fas fa-times"></i>
-						</p>
-					</div>
-				);
-			})}
+			{listOfTasks ? (
+				listOfTasks.length == 0 ? (
+					<p className="info">No tasks, add a task</p>
+				) : null
+			) : null}
 
-			{listOfTasks.length > 0 ? (
-				<p>{`${listOfTasks.length} item${
-					listOfTasks.length > 1 ? "s" : ""
-				} left`}</p>
+			{listOfTasks
+				? listOfTasks.map((item, index) => {
+						return (
+							<div className="todo" key={index}>
+								<p className="todo_content">{item.label}</p>
+								<p
+									className="todo_delete"
+									onClick={() => {
+										deleteTask(index);
+									}}>
+									<i className="fas fa-times"></i>
+								</p>
+							</div>
+						);
+				  })
+				: null}
+
+			{listOfTasks ? (
+				listOfTasks.length > 0 ? (
+					<p className="info">{`${listOfTasks.length} item${
+						listOfTasks.length > 1 ? "s" : ""
+					} left`}</p>
+				) : null
 			) : null}
 		</div>
 	);
